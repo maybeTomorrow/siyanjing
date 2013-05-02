b826b0a1da08690cd46dbbc39186b1c552b3a6ad
tidezyc <tide.zyc@gmail.com>
2013-05-02 15:56:08 +0800

    上传实现

---
+++ src/me/thinkjet/controller/UploadController.java
@@ -0,0 +1,18 @@
+package me.thinkjet.controller;
+
+import com.jfinal.core.Controller;
+import com.jfinal.ext.route.ControllerBind;
+
+/**
+ * CommonController
+ */
+@ControllerBind(controllerKey = "/upload", viewPath = "")
+public class UploadController extends Controller {
+	public void file() {
+	}
+
+	public void img() {
+
+	}
+
+}
