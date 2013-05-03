70cde200b39800981275d7d27b81e5ff3b482aa1
lin.xp <lin.xinpu@gmail.com>
2013-05-03 19:05:16 +0800

    更新了页面

--- src/me/thinkjet/controller/JobController.java
+++ src/me/thinkjet/controller/JobController.java
@@ -1,6 +1,7 @@
 package me.thinkjet.controller;
 
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 import me.thinkjet.auth.AuthManager;
 import me.thinkjet.interceptor.JobInterceptor;
@@ -22,9 +23,11 @@ public class JobController extends Controller {
 	@Before(CacheInterceptor.class)
 	@CacheName("job-index")
 	public void index() {
-		setAttr("joblist",
-				Job.dao.findByCache("job-index", "job",
-						SqlKit.sql("job.findListForJobIndexByViews")));
+		List<Job> list;
+		list=Job.dao.findByCache("job-index", "job",
+				SqlKit.sql("job.findListForJobIndexByViews"));
+		setAttr("joblist",list);
+		setAttr("count",list.size());
 	}
 
 	// 返回用户发布的所有招聘
@@ -44,7 +47,7 @@ public class JobController extends Controller {
 	@Before(JobValidator.class)
 	public void create() {
 		Job job = getModel(Job.class);
-		job.set("author", AuthManager.getSession(this).getUser().getLong("id"));
+		job.set("author", 5/*AuthManager.getSession(this).getUser().getLong("id")*/);
 		job.save();
 		render("index.html");
 	}
@@ -69,6 +72,7 @@ public class JobController extends Controller {
 
 	// 搜索岗位
 	public void search() throws UnsupportedEncodingException {
+		List<Job> list;
 		StringBuffer sql = new StringBuffer(
 				"select J.*,R.views,R.comments from job J left join job_record R on J.id=R.id where 1=1");
 		if (this.getPara("name") != null && !this.getPara("name").equals(""))
@@ -78,11 +82,16 @@ public class JobController extends Controller {
 		if (this.getPara("salary") != null
 				&& !this.getPara("salary").equals(""))
 			sql.append(" and J.salary=" + this.getPara("salary"));
-	/*	if (this.getPara("city") != null
+		if (this.getPara("city") != null
 				&& !this.getPara("city").equals(""))
-			sql.append(" and J.city =" +URLDecoder.decode(this.getPara("city"),"UTF-8"));*/
+			sql.append(" and J.city ='" +this.getPara("city")+"'");
+		if (this.getPara("company") != null
+				&& !this.getPara("company").equals(""))
+			sql.append(" and J.company like '%" +this.getPara("company")+"%'");
 		sql.append(" order by J.id asc limit 0,10");
-		setAttr("joblist", Job.dao.find(sql.toString()));
+		list=Job.dao.find(sql.toString());
+		setAttr("joblist", list);
+		setAttr("count",list.size());
 		render("index.html");
 	}
 }
