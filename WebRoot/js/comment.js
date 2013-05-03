26bd9a30c87eecf2e31c4098d6ff8eb9b3971ce6
tidezyc <tide.zyc@gmail.com>
2013-05-03 17:49:19 +0800

    评论系统更新

--- WebRoot/js/comment.js
+++ WebRoot/js/comment.js
@@ -3,24 +3,9 @@
  */
 $com_dis = $('#comment_display');
 $com_id = $com_dis.attr('rel');
-$.ajax({
-    type: "get",
-    url: "/comment/" + $com_id,
-    beforeSend: function(XMLHttpRequest){
-        $com_dis.append('<div id="com_loading"><img src="/images/loading.gif"/></div>');
-    },
-    success: function(data, status){
-        $com_dis.html(data);
-    },
-    complete: function(XMLHttpRequest, textStatus){
-        //HideLoading();
-    },
-    error: function(){
-        $com_dis.html('加载评论粗无');
-    }
-});
+$locked = false;
 
-function reply(uname, nickname){
+var reply = function(uname, nickname){
     $editor = $('#comment_add .editor');
     var at = document.createElement('button');
     at.name = '@{uname:' + uname + ',unickname:' + nickname + '}';
@@ -33,9 +18,62 @@ function reply(uname, nickname){
     $editor.focus();
 }
 
+var submit_form = function(){
+    if ($locked) {
+        return;
+    }
+    $locked = true;
+    $clone = $('#comment_add .editor').clone();
+    $button = $clone.children('button.metion');
+    $name = $button.attr('name');
+    $button.remove();
+    $.ajax({
+        type: "post",
+        url: "/comment/add/",
+        data: {
+            'target': $com_id,
+            'motion': $name,
+            'comment': $clone.html()
+        },
+        beforeSend: function(XMLHttpRequest){
+            $('#submit_info').html("").append('<img src="/images/loading.gif"/>');
+        },
+        success: function(data, status){
+            $('#submit_info').html("").append(data);
+            comment_load(false, $('#comment_page .selected').html());
+            $locked = false;
+        },
+        error: function(){
+            $('#submit_info').html("").append("评论系统出错");
+        }
+    });
+}
+
+var comment_load = function(cache, page){
+    $url = cache ? "/comment/" + $com_id + "-" + page : "/comment/update/" + $com_id + "-" + page;
+    $.ajax({
+        type: "get",
+        url: $url,
+        beforeSend: function(XMLHttpRequest){
+            $com_dis.append('<div id="com_loading"><img src="/images/loading.gif"/></div>');
+        },
+        success: function(data, status){
+            $com_dis.html(data);
+            $('#comment_add .submit').click(function(){
+                submit_form();
+            });
+            $('#comment_page a').click(function(e){
+                comment_load(true, $(this).attr('href').split('#')[1]);
+            });
+        },
+        error: function(){
+            $com_dis.html('加载评论错误');
+        }
+    });
+}
+
+comment_load(true, 1);// 初始化加载评论
 /*
-
  * 光标最后 END
-
  */
 
